e7d337dec887ddd32c3440d4f2b66a5ccab167c3
arthuraudieu <arthuraudieu@gmail.com>
2018-12-08 21:35:27 +0100

    this is the first version of the website

---
+++ JQ/jq.js
@@ -0,0 +1,5 @@
+ 
+    // faut penser à mettre le script dans un fichier à part si c'est possible. 
+$(document).ready(function(){
+                            $("#mainbanner").click(function(){$("#tohide_").hide();},function(){$("#panel").slideDown("slow");});
+                            });
