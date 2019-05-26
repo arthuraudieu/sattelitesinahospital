ead1023f753b8c29ac7729d95bb9cde87cf48ccb
arthuraudieu <45096176+arthuraudieu@users.noreply.github.com>
2019-05-26 08:52:46 +0200

    Update jq.js

--- JQ/jq.js
+++ JQ/jq.js
@@ -1,4 +1,4 @@
- 
+    // le test pour voir ce qu'est une branche=,
     // 26.05.2019:  il faudrait penser à mettre le script dans un fichier à part si c'est possible. 
 $(document).ready(function(){
                             $("#mainbanner").click(function(){$("#tohide_").hide();},function(){$("#panel").slideDown("slow");});
