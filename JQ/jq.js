475dc549ee442905618cd56c837e843647949541
arthuraudieu <45096176+arthuraudieu@users.noreply.github.com>
2019-05-26 08:52:03 +0200

    Update jq.js

--- JQ/jq.js
+++ JQ/jq.js
@@ -1,5 +1,5 @@
  
-    // faut penser à mettre le script dans un fichier à part si c'est possible. 
+    // 26.05.2019:  il faudrait penser à mettre le script dans un fichier à part si c'est possible. 
 $(document).ready(function(){
                             $("#mainbanner").click(function(){$("#tohide_").hide();},function(){$("#panel").slideDown("slow");});
                             });
